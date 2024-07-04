2ba98edcae4dab3c3fe708ad5326b18138c92e4c
Andrei Pokhila <andrei.pokhila@gmail.com>
2024-07-04 13:20:48 +0200

    binance-klines work

--- binance/futures/kline/exchange-connector/cmd/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/veska-io/streams-connectors/binance/futures/kline/exchange-connector/cmd/runner"
-)
-
-func main() {
-	runner.MustRun()
-}
--- main/futures/exchanges-events-loader/cmd/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	runner "github.com/veska-io/streams-connectors/main/futures/exchanges-events-loader/cmd/runner"
-)
-
-func main() {
-	runner.MustRun()
-}
