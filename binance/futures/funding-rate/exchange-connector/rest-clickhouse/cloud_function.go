a1437f1dc4b677dde12c295f8d19ab98eb84abdd
Andrei Pokhila <andrei.pokhila@gmail.com>
2024-07-16 12:20:37 +0200

    binance funding rate

---
+++ binance/futures/funding-rate/exchange-connector/rest-clickhouse/cloud_function.go
@@ -0,0 +1,20 @@
+package kline_parser
+
+import (
+	"context"
+
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+	"github.com/cloudevents/sdk-go/v2/event"
+
+	"github.com/veska-io/streams-connectors/binance/futures/funding-rate/exchange-connector/rest-clickhouse/cmd/runner"
+)
+
+func init() {
+	functions.CloudEvent("RunMain", RunConnector)
+}
+
+func RunConnector(ctx context.Context, e event.Event) error {
+	runner.MustRun()
+
+	return nil
+}
---
+++ binance/futures/funding-rate/exchange-connector/rest-clickhouse/src/consumer/consumer_test.go
@@ -0,0 +1,29 @@
+package consumer_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/adshao/go-binance/v2"
+	"github.com/veska-io/streams-connectors/binance/futures/funding-rate/exchange-connector/rest-clickhouse/cmd/runner"
+)
+
+func TestStrangeRequest(t *testing.T) {
+	start := time.Date(2020, 1, 1, 1, 0, 0, 0, time.UTC)
+	end := time.Date(2021, 10, 20, 1, 0, 0, 0, time.UTC)
+
+	restClient := binance.NewFuturesClient("", "")
+	fRate, err := restClient.NewFundingRateHistoryService().Symbol("OXTUSDT").Limit(1000).
+		StartTime(start.UnixMilli()).EndTime(end.UnixMilli()).Do(context.Background())
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	t.Log(len(fRate))
+}
+
+func TestRun(t *testing.T) {
+	runner.MustRun()
+}
