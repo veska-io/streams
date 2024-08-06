a3be28d3af54d411b56d7d458f4159b09eecd2f0
Andrei Pokhila <andrei.pokhila@gmail.com>
2024-08-06 15:18:07 +0200

    binance oi

--- binance/futures/open-interest/exchange-connector/rest-clickhouse/cmd/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/veska-io/streams-connectors/binance/futures/open-interest/exchange-connector/rest-clickhouse/cmd/runner"
-)
-
-func main() {
-	runner.MustRun()
-}
